22d42cad413784f084c32db97f373bbd3bc6117e
emre.ozhan <emreozhan71@gmail.com>
2019-03-02 02:42:34 +0300

    send data add

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello YeeLight!");
+
+        byte[] sendData = ("M-SEARCH * HTTP/1.1\r\n" +
+                "HOST: 239.255.255.250:1982\r\n" +
+                "MAN: \"ssdp:discover\"\r\n" +
+                "ST: wifi_bulb").getBytes();
     }
 }
